8d95c4952a4c8e09b8a65cea2121f0f41fa2bf10
Ana Pao Minchaca <42215143+anapao-minchaca@users.noreply.github.com>
2023-01-23 18:49:42 -0600

    upload python exercises

---
+++ madlibs/madlibs.py
@@ -0,0 +1,13 @@
+# youtuber = "Cody Ko"
+
+
+# # a few ways to concatenate
+# print("subscribing to " + youtuber)
+# print("subscribing to {}".format(youtuber))
+# print(f"subscribing to {youtuber}")
+
+
+adj = input("Adjective: ")
+madlib = f"Computer programming is so {adj}! "
+
+print(madlib)
